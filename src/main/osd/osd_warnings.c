b8a1955a5eab9e89229eab8e902de7a5b1257441
ctzsnooze <chris.thompson@sydney.edu.au>
2021-02-28 00:10:03 +1100

    original code
    from Manuel Berthomier loutwice on github

--- src/main/osd/osd_warnings.c
+++ src/main/osd/osd_warnings.c
@@ -109,6 +109,19 @@ void renderOsdWarning(char *warningText, bool *blinking, uint8_t *displayAttr)
         }
     }
 
+    // Show warning if no altitude limitation applicable
+            if (getThrottleLimitationStatus() == 2) {
+                tfp_sprintf(warningText, "NO ALTI LIM");
+                *displayAttr = DISPLAYPORT_ATTR_WARNING;
+                *blinking = true;
+                return;
+            } else if(getThrottleLimitationStatus() == 1) {
+                tfp_sprintf(warningText, "ALTI LIM");
+                *displayAttr = DISPLAYPORT_ATTR_WARNING;
+                *blinking = true;
+                return;
+            }
+
 #ifdef USE_DSHOT
     if (isTryingToArm() && !ARMING_FLAG(ARMED)) {
         int armingDelayTime = (getLastDshotBeaconCommandTimeUs() + DSHOT_BEACON_GUARD_DELAY_US - currentTimeUs) / 1e5;
